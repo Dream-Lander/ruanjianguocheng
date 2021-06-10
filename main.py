e0715140c474410cb5a566332d4416a59af5b557
zhangshiyi <1906581992@qq.com>
2021-06-10 10:28:45 +0800

    Second commit

--- main.py
+++ main.py
@@ -1,4 +1,21 @@
+import unittest
+
 from selenium import webdriver
-browser=webdriver.Edge()
-browser.get('http://localhost:8888')
-assert 'Django' in browser.title
+browser=webdriver.Edge('msedgedriver.exe')
+browser.get('http://localhost:8000')
+assert '88' in browser.title,"Browser title was"+browser.title
+
+class NewVisitorTest(unittest.TestCase):
+    def setUp(self):
+        self.browser=webdriver.Edge('msedgedriver.exe')
+
+    def tearDown(self):
+        self.browser.quit()
+
+    def test_can_start_a_list_and_retrieve_it_later(self):
+        self.browser.get('http://localhost:8000')
+        self.assertIn('Django',self.browser.title)
+        self.fail('Finish the test!')
+
+if __name__ == '__main__':
+    unittest.main(warnings='ignore')
